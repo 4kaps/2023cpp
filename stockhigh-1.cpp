ee9f4830520570655f7584127f538f4a95302d0a
4kaps <mmresult2@kookmin.ac.kr>
2023-04-12 15:18:05 +0900

    2

--- stockhigh-1.cpp
+++ stockhigh-1.cpp
@@ -2,39 +2,29 @@
 using namespace std;
 
 int main(){
-    int t, n, num1, num2, num3, temp, first_num;
-    bool first_continue = false;
+    int t, n, count, num1, num2;
+    bool canCount;
     cin >> t;
-    int count = 0;
     for(int i=0; i<t; i++){
-        cin >> n >> num1 >> num2;
-        for(int j=0; j<n-2; j++){
-            cin >> temp;
-            num3 = temp;
-            if(j==0){
-                first_num = num1;
-            }
-            if((num1 <= num2) && (num3 < num2)){
-                count+=1;
-            }
-            if((first_num == num1 == num2 == num3) || ((num1 == num2) && (j==0))){
-                first_continue = true;
-            }
-            if((num1 == num2) && (num3 < num2) && (num1 == first_num) && (first_continue)){
-                count-=1;
-                first_continue = false;
+        canCount = false;
+        count = 0;
+        num1 = 0;
+        cin >> n;
+        for(int j=0; j<n; j++){
+            cin >> num2;
+            if(num2 > num1){
+                num1 = num2;
+                if(j!=0){
+                    canCount = true;
+                }
+            }else if(num1 > num2 && canCount){
+                num1 = num2;
+                count++;
+                canCount = false;
             }
             num1 = num2;
-            num2 = num3;
         }
         cout << count << endl;
-        count = 0;
-        first_continue = false;
     }
 }
-/*
-first_num : 첫 세 수를 비교할때, num1 값
-first_continue : 첫 값이 이어지는 5 5 5 / 5 5 3(j=0) -> true
-*/
-
 
---
+++ twosquare.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int get
+
+int main(){
+    int k;
+    cin >> k;
+    int px1, py1, qx1, qy1, px2, py2, qx2, qy2;
+    while(k-->0){
+        cin >> px1 >> py2 >> qx1 >> qy1;
+        cout << endl;
+        cin >> px2 >> py2 >> qx2 >> qy2;
+    }
+}
